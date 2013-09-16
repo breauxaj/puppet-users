92597aba5e1cdcd72cb884e02c4a31c61c705616
Adam Breaux <breauxaj@gmail.com>
2013-09-16 08:28:58 -0400

    enforce home folder permissions/ownership

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,14 @@
 class users {
+  $path = $operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => [ '/home' ],
+  }
+
+  file { $path:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
 
 }
 
