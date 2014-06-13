d9a8de29a12c97e5c8f2a1c025d09912a8a64ecb
Adam Breaux <breauxaj@gmail.com>
2014-06-13 10:02:42 -0400

    clean out for now

--- lib/puppet/type/my_cnf.rb
+++ lib/puppet/type/my_cnf.rb
@@ -3,20 +3,4 @@ Puppet::Type.newtype(:my_cnf) do
 
   ensurable
 
-  newparam(:name) do
-    desc ""
-
-    isnamevar
-
-  end
-
-  newproperty(:user) do
-    desc "The user account in which the SSH key should be installed.
-    The resource will automatically depend on this user."
-  end
-
-  autorequire(:user) do
-    should(:user) if should(:user)
-  end
-
 end
