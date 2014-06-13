02a159af215e190e8d3b4562aa94f55d7eb4e1a5
Adam Breaux <breauxaj@gmail.com>
2014-06-13 10:45:42 -0400

    Maybe?

---
+++ lib/puppet/provider/my_cnf/ruby.rb
@@ -0,0 +1,31 @@
+require 'fileutils'
+
+Puppet::Type.type(:my_cnf).provide(:ruby) do
+  desc "Manage the .my.cnf file"
+
+  commands :cat => 'cat'
+
+  filename = "/home/" + resource[:name] + ".my.cnf"
+
+  def create
+    content = "[client]\nuser = " + resource[:dbuser] + "\npassword = \"" + resource[:dbpass] + "\"\nhost = " + resource[:dbhost] + "\n"
+
+    File.open(filename, "wb") do |file|
+      file.write(content)
+    end
+
+  end
+
+  def destroy
+    cat(['/dev/null', filename])
+  end
+
+  def exists?
+    File.zero?(filename)
+  end
+
+  autorequire(:file) do
+    ["/home", filename]
+  end
+
+end
