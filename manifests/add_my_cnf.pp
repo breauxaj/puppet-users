fe1a6dd0692cc75b33b529f03931f3ffcc8f13a1
Adam Breaux <breauxaj@gmail.com>
2014-06-13 11:00:46 -0400

    handle at the wrapper?

--- manifests/add_my_cnf.pp
+++ manifests/add_my_cnf.pp
@@ -1,15 +1,15 @@
 define users::add_my_cnf (
-  $username,
   $dbuser,
   $dbpass,
   $dbhost
 ) {
   my_cnf { $name:
-    ensure  => present,
-    dbuser  => $dbuser,
-    dbpass  => $dbpass,
-    dbhost  => $dbhost,
-    require => File["/home/${username}/.my.cnf"],
+    ensure   => present,
+    username => $name,
+    dbuser   => $dbuser,
+    dbpass   => $dbpass,
+    dbhost   => $dbhost,
+    require  => File["/home/${username}/.my.cnf"],
   }
 
 }
