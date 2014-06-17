9e7052727a2dc51533dc21162db78de3bca1f568
Adam Breaux <breauxaj@gmail.com>
2014-06-17 17:23:28 -0400

    handle root user

--- manifests/add_my_cnf.pp
+++ manifests/add_my_cnf.pp
@@ -3,12 +3,31 @@ define users::add_my_cnf (
   $dbpass,
   $dbhost
 ) {
-  my_cnf { $name:
-    ensure   => present,
-    dbuser   => $dbuser,
-    dbpass   => $dbpass,
-    dbhost   => $dbhost,
-    require  => File["/home/${name}/.my.cnf"],
+  if $name =~ /^root$/ {
+    file { "/${name}/.my.cnf":
+      ensure  => present,
+      owner   => 0,
+      group   => 0,
+      mode    => '0600',
+    }
+
+    my_cnf { $name:
+      ensure   => present,
+      dbuser   => $dbuser,
+      dbpass   => $dbpass,
+      dbhost   => $dbhost,
+      require  => File["/${name}/.my.cnf"],
+    }
+
+  } else {
+    my_cnf { $name:
+      ensure   => present,
+      dbuser   => $dbuser,
+      dbpass   => $dbpass,
+      dbhost   => $dbhost,
+      require  => File["/home/${name}/.my.cnf"],
+    }
+
   }
 
 }
