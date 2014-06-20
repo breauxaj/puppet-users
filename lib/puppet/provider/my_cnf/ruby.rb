c6e11c4be538442b9086b9c48c256c027af22d2c
Adam Breaux <breauxaj@gmail.com>
2014-06-20 10:46:35 -0400

    meaningful name for def

--- lib/puppet/provider/my_cnf/ruby.rb
+++ lib/puppet/provider/my_cnf/ruby.rb
@@ -3,25 +3,25 @@ Puppet::Type.type(:my_cnf).provide(:ruby) do
 
   commands :cat => 'cat'
 
-  def my_file(str)
+  def my_cnf_file(str)
     return File.expand_path("~" + str + "/.my.cnf" )
   end
 
   def create
     content = "[client]\nuser = " + @resource[:dbuser] + "\npassword = '" + @resource[:dbpass] + "'\nhost = " + @resource[:dbhost] + "\n"
 
-    File.open(my_file(@resource[:name]), "w") do |file|
+    File.open(my_cnf_file(@resource[:name]), "w") do |file|
       file.puts(content)
     end
 
   end
 
   def destroy  
-    cat(['/dev/null', '>', my_file(@resource[:name])])
+    cat(['/dev/null', '>', my_cnf_file(@resource[:name])])
   end
 
   def exists?
-    ! File.zero?(my_file(@resource[:name]))
+    ! File.zero?(my_cnf_file(@resource[:name]))
   end
 
 end
